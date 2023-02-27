3d8194711299459d032bda894d2ae4e6bf2271f0
MarioKarv <mariook0204@gmail.com>
2023-02-27 16:52:29 +0200

    1 завдання виконано

---
+++ src/js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+const photosMarkup = createPhotosMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', photosMarkup)
+
+gallery.addEventListener('click', onPhotoClick)
+
+function createPhotosMarkup(photos) {
+  return photos.map(({ preview, original, description }) => {
+    return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`;
+  }).join('')
+}
+
+// function onPhotoClick(event) {
+//   event.preventDefault();
+//   const instance = basicLightbox.create(`
+// 		<img width="1400" height="900" src="${event.target.dataset.source}">
+// 	`);
+//   instance.show();
+// }
+
+const instance = basicLightbox.create(
+  `<img width="1140" height="720" src="#">`,
+  {
+    onShow: () => {
+      window.addEventListener("keydown", onEscPress);
+    },
+    onClose: () => {
+      window.removeEventListener("keydown", onEscPress);
+    },
+  }
+);
+
+function onPhotoClick(event) {
+  event.preventDefault();
+  instance.element().querySelector("img").src = event.target.dataset.source;
+  instance.show();
+}
+
+function onEscPress(event) {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
